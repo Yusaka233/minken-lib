998a0f837d548e0003b15e0b73bb4568f24e0294
Yusaka233 <1448496835@qq.com>
2024-06-06 11:53:30 +0800

    1

---
+++ src/components/Switch/demos/index5.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Switch } from 'wskco';
+export default function index1() {
+  return (
+    <div>
+      <Switch style={{ marginRight: '10px' }} loading />
+      <Switch defaultChecked loading />
+    </div>
+  );
+}
