012bfc150b9df9971c6e85503edbfd2f721490b2
Yusaka233 <1448496835@qq.com>
2024-06-07 18:49:23 +0800

    基础功能

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
+  title: 'MINKEN-LIB',
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'MINKEN',
   },
+  mode: 'site',
+  base:'/MINKEN/',
+  publicPath:'/MINKEN/',
+  exportStatic: {},
   resolve: {
     atomDirs: [
       { type: 'component', dir: 'src/components'},
